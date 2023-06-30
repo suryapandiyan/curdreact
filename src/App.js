47d55d3602ab14172fad72716d94071394128148
suryapandiyan <surya.surya256@gmail.com>
2023-07-01 00:56:55 +0530

    finally completed curd

---
+++ src/App.js
@@ -0,0 +1,33 @@
+import logo from './logo.svg';
+import './App.css';
+import Sidebar from './Sidebar';
+import Topbar from './Topbar';
+import "./sb-admin-2.min.css";
+import Dashboard from './Dashboard';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Login from './Login';
+import Userlist from './Userlist';
+import Portal from './Portal';
+import UserCreate from './UserCreate';
+import UserView from './UserView';
+import UserEdit from './UserEdit';
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<Login />} />
+
+        <Route path='/portal' element={<Portal />}>
+          <Route path='dashboard' element={<Dashboard />} />
+          <Route path='user-list' element={<Userlist />} />
+          <Route path='create-user' element={<UserCreate />} />
+          <Route path='user-view/:id' element={<UserView />} />
+          <Route path='user-edit/:id' element={<UserEdit />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
